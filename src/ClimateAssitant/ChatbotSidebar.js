64c9c21bd2e119bae1ece67e8f11e99df0f4fc27
syedshahhussain19288@gmail.com <shahxhussain>
2025-02-24 08:43:45 +0500

    together.ai API added

--- src/ClimateAssitant/ChatbotSidebar.js
+++ src/ClimateAssitant/ChatbotSidebar.js
@@ -7,25 +7,33 @@ const Sidebar = ({ setApiKey }) => {
   return (
     <div className="sidebar">
       <Container className="d-flex flex-column align-items-center justify-content-center h-100">
-         {/* API Key Input */}
-         <Form className="mt-3">
+        {/* API Key Input */}
+        <Form className="mt-3 text-center">
           <Form.Group>
-            <Form.Label className="text-white">Enter API Key</Form.Label>
+            <Form.Label className="text-white">Enter Together.ai API Key</Form.Label>
             <Form.Control
               type="password"
               placeholder="Together AI API Key"
               onChange={(e) => setApiKey(e.target.value)}
             />
           </Form.Group>
+          {/* API Key Link */}
+          <a 
+            href="https://together.ai/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="api-link"
+          >
+            Get your API here
+          </a>
         </Form>
+
         <div className="vertical-text">
           C<br />L<br />I<br />M<br />A<br />T<br />E<br />
           C<br />H<br />A<br />N<br />G<br />E<br /><br />
           C<br />H<br />A<br />T<br />B<br />O<br />T
         </div>
         <FaLeaf className="leaf-icon" />
-        
-       
       </Container>
     </div>
   );
